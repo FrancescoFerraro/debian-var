de1567fd34793c64b23f3924467583c164b7fb3b
FrancescoFerraro <francesco.f@variscite.com>
2023-09-21 10:29:49 +0200

    imx8mp-var-dart: u-boot-variscite: Update to develop branch
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- variscite/imx8mp-var-dart/imx8mp-var-dart.sh
+++ variscite/imx8mp-var-dart/imx8mp-var-dart.sh
@@ -50,9 +50,9 @@ readonly G_IMXBOOT_PATCHES="${G_VARISCITE_PATH}/${MACHINE}/imx-boot-tools/imx-bo
 
 # U-Boot
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
-readonly G_UBOOT_GIT="https://github.com/varigit/uboot-imx.git"
+readonly G_UBOOT_GIT="https://github.com/FrancescoFerraro/uboot-imx.git"
 readonly G_UBOOT_BRANCH="lf_v2023.04_var01"
-readonly G_UBOOT_REV="341b473039306055c34e154af070e941d7cafb14"
+readonly G_UBOOT_REV="c24b0c4a78a8b387df64577a0fb0914f3144071b"
 G_UBOOT_DEF_CONFIG_MMC="imx8mp_var_dart_defconfig"
 readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 readonly UBOOT_FW_UTILS_DIR="${G_META_VARISCITE_BSP_IMX_SRC_DIR}/recipes-bsp/u-boot/u-boot-fw-utils/${MACHINE}"
